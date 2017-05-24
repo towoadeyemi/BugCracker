fea0079fc2771046cd87ec986d9d7eab5678969e
c7167916 <c7167916@JG109-P18>
2017-05-24 20:18:05 +0100

    Add project files.

---
+++ BugFix/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BugFix.Global" Language="C#" %>
---
+++ BugFix/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugFix.Startup))]
+namespace BugFix
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
